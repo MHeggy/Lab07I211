20373be1ce106eb7057093f68f811ed3e86f9aa8
chrforb <143653990+chrforb@users.noreply.github.com>
2023-10-12 11:40:14 -0400

    Add files via upload
    toString() and getPaymentAmount() need finished

--- hourly_employee.class.php
+++ hourly_employee.class.php
@@ -1,7 +1,40 @@
 <?php
 /**
- * Author: Miles Hegeduis
+ * Author: Christian Forbes
  * Date: 10/10/2023
  * File Name: hourly_employee.class.php
- * Description:
+ * Description: defines HourlyWorker class.
  */
+
+class HourlyEmployee extends Employee {
+    //class private attributes
+    private $wage, $hours;
+
+    public function __construct($name, $title, $years, $salary, $wage, $hours)
+    {
+        parent::__construct($name, $title, $years, $salary);
+        $this->wage=$wage;
+        $this->hours=$hours;
+    }
+
+    //getter for wage
+    public function getWage() {
+        return $this->wage;
+    }
+
+    //getter for hours
+    public function getHours() {
+        return $this->hours;
+    }
+
+    //implement abstract methods from Employee
+    public function getPaymentAmount()
+    {
+        // TODO: Implement getPaymentAmount() method.
+    }
+
+    public function toString()
+    {
+        // TODO: Implement toString() method.
+    }
+}
--- invoice.class.php
+++ invoice.class.php
@@ -1,7 +1,68 @@
 <?php
 /**
- * Author: Miles Hegeduis
+ * Author: Christian Forbes
  * Date: 10/10/2023
  * File Name: invoice.class.php
- * Description:
+ * Description: define Invoice class. This class includes get methods for all private attributes, invoice count, to string.
  */
+
+class Invoice implements Payable {
+    //private attributes
+    private $part_number, $part_description, $quantity, $price_per_item;
+
+    //static attribute for invoice count
+    static private $invoice_count = 0;
+
+    //class constructor using all attributes
+    public function __construct($part_number, $part_description, $quantity, $price_per_item, $invoice_count)
+    {
+        $this->part_number = $part_number;
+        $this->part_description = $part_description;
+        $this->quantity = $quantity;
+        $this->price_per_item = $price_per_item;
+        $this->invoice_count = $invoice_count;
+        //increment invoice count each time a new object is made
+        self::$invoice_count++;
+    }
+
+    //getter for price per item
+    public function getPartNumber()
+    {
+        return $this->part_number;
+    }
+
+    //getter for price per item
+    public function getPartDescription()
+    {
+        return $this->part_description;
+    }
+
+    //getter for price per item
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    //getter for price per item
+    public function getPricePerItem()
+    {
+        return $this->price_per_item;
+    }
+
+    //get incremented invoice count from self
+    public function getInvoiceCount() {
+        return self::$invoice_count;
+    }
+
+    //implement methods from Payable interface
+    public function getPaymentAmount()
+    {
+        // TODO: Implement getPaymentAmount() method.
+    }
+
+    public function toString()
+    {
+        // TODO: Implement toString() method.
+    }
+
+}
